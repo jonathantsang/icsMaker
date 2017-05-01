3dd1829b37623fb807993bf1b7f7edee8569c71c
jonathantsang <j26tsang@gmail.com>
2017-05-01 17:46:32 -0400

    basic ics appears

--- src/bg/background.js
+++ src/bg/background.js
@@ -3,7 +3,10 @@ chrome.browserAction.onClicked.addListener(function (tab) { //Fired when User Cl
 	console.log("Script Executed .. ");
 	function exportToFile() {
     var myString = "BEGIN:VCALENDAR\nVERSION:2.0\nBEGIN:VEVENT\nDTSTART:20170517T233000Z\nDTEND:20170518T010000Z\nSUMMARY:Your Event\nDESCRIPTION:\nLOCATION:Your Location\nEND:VEVENT\nEND:VCALENDAR";
-    window.open('data:text/;charset=utf-8,' + escape(myString));
+    var selected = window.getSelection().toString();
+    myString += selected;
+    console.log(selected);
+    window.open('data:ics/;charset=utf-8,' + escape(myString));
 }
 	exportToFile();
 });
